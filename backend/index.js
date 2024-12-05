c25acd50af391d06e91b87ab8fe0b0e679dbc022
Dynocoder <sauravprashar21@gmail.com>
2024-12-05 13:49:07 -0500

    implemented load, save, clear

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import redis from "redis";
 
@@ -10,24 +11,52 @@ const client = redis.createClient({
 });
 const app = express();
 const port = process.env.APP_PORT || 3001;
+app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World\n");
+// app.get("/", (req, res) => {
+//   res.send("Hello World\n");
+// });
+
+app.get("/load", async (req, res) => {
+  try {
+    let exists = await client.exists("todo_list");
+    let todo_list;
+    if (exists == 0) {
+      todo_list = [];
+      await client.set("todo_list", JSON.stringify(todo_list));
+    } else {
+      todo_list = await client.get("todo_list");
+    }
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+
+  console.log(todo_list);
+  res.send(JSON.stringify(todo_list));
 });
 
-//TODO: get the todo list from the redis db
-app.get("/load", (req, res) => {
-  res.send("loading shit");
+app.post("/save", async (req, res) => {
+  try {
+    let todo_list = req.body.todo_list;
+
+    await client.set("todo_list", JSON.stringify(todo_list));
+
+    res.send({ status: "save successful" });
+  } catch (err) {
+    console.log(err);
+    res.send(err);
+  }
 });
 
-//TODO: save the todo list from the redis db
-app.get("/save", (req, res) => {
-  res.send("loading shit");
-});
-
-//TODO: clear the todo list(s) from the redis db
-app.get("/clear", (req, res) => {
-  res.send("loading shit");
+app.get("/clear", async (req, res) => {
+  try {
+    await client.set("todo_list", JSON.stringify([]));
+    res.send({ status: "clear successful" });
+  } catch (err) {
+    console.log(err);
+    res.send(err);
+  }
 });
 
 client.connect();
